e982419c96fcd78a97a399d8607585e456402fec
Tester86 <tester86t@gmail.com>
2019-10-23 20:58:40 +0200

    Adding first complete version of passfactory!

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
-from setuptools import setup
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
+from distutils.core import setup
 setup(
-    name="passfactory",
-    version="1.0",
-    description="A random password generator which includes various utils to work with them",
-    py_modules=["passgen"],
-    package_dir={"":"src"},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
-        "Operating System :: OS Independent"
-    ],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Tester86/passfactory",
-    author="Tester86",
-    author_email="tester86t@gmail.com",
+  name = 'passfactory',         # How you named your package folder (MyLib)
+  packages = ['src'],   # Chose the same as "name"
+  version = '0.0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Save password generator and tool box to operate them',   # Give a short description about your library
+  author = 'Tester86',                   # Type in your name
+  author_email = 'tester86t@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/Tester86/passfactory',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Tester86/passfactory/passfactory.py/v_01.tar.gz',    # I explain this later on
+  keywords = ['PASSWORD', 'TOOLBOX', 'GENERATOR'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'pyperclip',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
